abb75f2eae76a002e29a47955e753e3c41b832aa
marcellino.palerme <marcellino.palerme@inra.fr>
2021-03-23 08:32:50 +0100

    in multiviews, I manage split train and test

--- processing/python/multi_view.py
+++ processing/python/multi_view.py
@@ -9,28 +9,36 @@ import numpy as np
 from tools_file import file_list
 from os.path import join
 from PIL import Image
+from sklearn.model_selection import train_test_split
+
 
 # Define size of image
 img_height = 224
 img_width = 224
 
 # Define directory where take image
-Big = '/home/port-mpalerme/Documents/Atipical/Traitement/photos/deep/fit/Big'
-Mac = '/home/port-mpalerme/Documents/Atipical/Traitement/photos/deep/fit/Mac'
-Myc = '/home/port-mpalerme/Documents/Atipical/Traitement/photos/deep/fit/Myc'
-Pse = '/home/port-mpalerme/Documents/Atipical/Traitement/photos/deep/fit/Pse'
-Syl = '/home/port-mpalerme/Documents/Atipical/Traitement/photos/deep/fit/Syl'
+Big = '/home/genouest/inra_umr1349/mpalerme/deep/photos/Big_bdb_cut2_resize'
+Mac = '/home/genouest/inra_umr1349/mpalerme/deep/photos/Mac_bdb_cut2_resize'
+Myc = '/home/genouest/inra_umr1349/mpalerme/deep/photos/Myc_bdb_cut2_resize'
+Pse = '/home/genouest/inra_umr1349/mpalerme/deep/photos/Pse_bdb_cut2_resize'
+Syl = '/home/genouest/inra_umr1349/mpalerme/deep/photos/Syl_bdb_cut2_resize'
 recto = 'recto'
 verso = 'verso'
 
-all_photos_recto = []
-all_photos_verso = []
-all_labels = []
+train_photos_recto = []
+train_photos_verso = []
+train_labels = []
+test_photos_recto = []
+test_photos_verso = []
+test_labels = []
 
 # Take all images and labels
 for label, where in enumerate([Big, Mac, Myc, Pse, Syl]):
     files_verso = file_list(join(where, verso))
     files_recto = file_list(join(where, recto))
+    index = 0
+    train_test = train_test_split(range(len(files_verso)),
+                                  test_size=0.3)
     for file_r, file_v in zip(files_recto, files_verso):
         try:
             # Read image recto
@@ -38,16 +46,28 @@ for label, where in enumerate([Big, Mac, Myc, Pse, Syl]):
             # Read image verso
             img_ver = Image.open(join(where, verso, file_v))
         except IOError :
+            index = index + 1
             continue
-        # Transform image to array and add array
-        all_photos_recto.append(img_to_array(img_rec))
-        all_photos_verso.append(img_to_array(img_ver))
-        # Add Image's label
-        all_labels.append(label)
+        if index in train_test[0]:
+            # Transform image to array and add array
+            train_photos_recto.append(img_to_array(img_rec))
+            train_photos_verso.append(img_to_array(img_ver))
+            # Add Image's label
+            train_labels.append(label)
+        else:
+            # Transform image to array and add array
+            test_photos_recto.append(img_to_array(img_rec))
+            test_photos_verso.append(img_to_array(img_ver))
+            # Add Image's label
+            test_labels.append(label)
+        index = index + 1
 
-all_photos_recto = np.array(all_photos_recto)
-all_photos_verso = np.array(all_photos_verso)
-all_labels = np.array(all_labels)
+train_photos_recto = np.array(train_photos_recto)
+train_photos_verso = np.array(train_photos_verso)
+train_labels = np.array(train_labels)
+test_photos_recto = np.array(test_photos_recto)
+test_photos_verso = np.array(test_photos_verso)
+test_labels = np.array(test_labels)
 
 # Define the process for augmentation data
 data_augmentation_r = tf.keras.Sequential([
@@ -104,23 +124,31 @@ model = tf.keras.Model(inputs=[data_augmentation_r.input,
                                data_augmentation_v.input],
                        outputs=model_final)
 
+#Callback
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath='/home/genouest/inra_umr1349/mpalerme/model_multi_view_best.hd5',
+    monitor='val_accuracy',
+    mode='max',
+    save_best_only=True)
+
 # Compile the Network
 model.compile(
   optimizer='adam',
   loss='sparse_categorical_crossentropy',
-  metrics=['accuracy'])
+  metrics=['accuracy'],
+  callbacks=[model_checkpoint_callback])
 
 print(model.summary())
 
 
 # Training the network
 model.fit(
-  x= [all_photos_recto, all_photos_verso], y=all_labels,
-  validation_split = 0.3,
+  x= [train_photos_recto, train_photos_verso], y=train_labels,
+  validation_data = ([test_photos_recto, test_photos_verso], test_labels),
   epochs=150,
   verbose=2
 )
 
 # Save the network
-model.save("./model.hd5")
+model.save("/home/genouest/inra_umr1349/mpalerme/model_multi_view_last.hd5")
 
