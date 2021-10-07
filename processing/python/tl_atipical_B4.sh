51f4ea635f36e11b93266216fac7d6867cc0a94d
mpalerme <mpalerme@inra.fr>
2021-10-07 10:17:17 +0200

    create specific script to EfficientNet B4

--- processing/python/tl_atipical_B4.sh
+++ processing/python/tl_atipical_B4.sh
@@ -3,6 +3,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH -p gpu
 #SBATCH --mem=50G
+#SBATCH --output=.report/$(date "+%Y%m%d_%H%M%S")results_tl_atipical_B4.txt
 
 . /softs/local/env/envconda.sh
 conda activate /home/genouest/inra_umr1349/mpalerme/conda
