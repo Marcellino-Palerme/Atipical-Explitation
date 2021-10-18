f867988f0599e7153453223554859d3384f12bd2
mpalerme <mpalerme@inra.fr>
2021-10-18 14:41:18 +0200

    correct push correct command line in temporary file

--- processing/python/tl_atipical_B4.sh
+++ processing/python/tl_atipical_B4.sh
@@ -7,11 +7,11 @@ temp_file="${my_date}_temp.txt"
 
 #Add all line to run deep learning
 echo '#!/bin/bash' >> $temp_file
-echo "\#SBATCH --job-name=tl_atipical_B4_${my_date}" >> $temp_file
+echo "#SBATCH --job-name=tl_atipical_B4_${my_date}" >> $temp_file
 echo '#SBATCH --gres=gpu:1' >> $temp_file
 echo '#SBATCH -p gpu' >> $temp_file
 echo '#SBATCH --mem=50G' >> $temp_file
-echo "\#SBATCH --output=./report/${my_date}_tl_atipical_B4/${my_date}_results_tl_atipical_B4.txt" >> $temp_file
+echo "#SBATCH --output=./report/${my_date}_tl_atipical_B4/${my_date}_results_tl_atipical_B4.txt" >> $temp_file
 
 echo . /softs/local/env/envconda.sh >> $temp_file
 echo conda activate /home/genouest/inra_umr1349/mpalerme/conda >> $temp_file
