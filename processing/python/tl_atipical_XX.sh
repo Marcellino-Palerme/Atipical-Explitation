067614abc6bf317b81034858ed09ce5e1d00f76c
mpalerme <mpalerme@inra.fr>
2021-12-07 09:51:26 +0100

    delete redirection command to unexisting file

--- processing/python/tl_atipical_XX.sh
+++ processing/python/tl_atipical_XX.sh
@@ -10,7 +10,7 @@ my_date=$(date "+%Y%m%d_%H%M%S")
 #Create directory of report
 mkdir -p "./report/${my_date}_tl_atipical_XX"
 
-. /softs/local/env/envconda.sh >> $temp_file
+. /softs/local/env/envconda.sh
 conda activate /home/genouest/inra_umr1349/mpalerme/conda
 python tl_atipical_XX.py ${my_date} B3 >> "./report/${my_date}_tl_atipical_XX/${my_date}_B3"
 python tl_atipical_XX.py ${my_date} B4 >> "./report/${my_date}_tl_atipical_XX/${my_date}_B4"
